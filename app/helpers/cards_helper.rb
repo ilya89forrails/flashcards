7e852826af2d4ae2d877b1f3b89e057079447f48
ilya <ilya89forrails@gmail.com>
2016-07-17 18:50:26 +0900

    lesson3

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,8 +1,5 @@
 module CardsHelper
-
   def card_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
-
-
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,20 +1,19 @@
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, :review_date, presence: true 
-  
-  before_save :add_days, on: :create
-  before_save :same_words, on: :create
+  validates :original_text, :translated_text, :review_date, presence: true
+ 	validate :same_words, on: :create
 
+  before_save :add_days, on: :create
+  
 
   protected
 
   def add_days
-  	self.review_date = self.review_date + 3.days
+    self.review_date = review_date + 3.days
   end
 
   def same_words
-    if (self.original_text.downcase == self.translated_text.downcase)
-    	errors.add(:translated_text, "Translated text cannot equal original text")
+    if original_text.casecmp(translated_text.downcase).zero?
+      errors.add(:translated_text, 'Translated text cannot equal original text')
     end
   end
-
 end
