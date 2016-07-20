e2b5c26d4cba5579109ae2b7b167e21e74e96d00
ilya <ilya89forrails@gmail.com>
2016-07-20 21:40:52 +0900

    lesson4+additions

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,21 +14,22 @@ def one_page(full_path)
 
   current_page.css('tr').each do |word|
     original_word = word.css('td.bigLetter').text
-
     translated_word = word.css('td')[2].text
-
-    Card.create(original_text: original_word, translated_text: translated_word, review_date: Date.today)
+    Card.create(original_text: original_word, translated_text: translated_word, review_date: Date.today) unless translated_word.nil?
   end
 end
 
 def all_pages
-  path_begin = 'http://www.languagedaily.com/learn-german/vocabulary/common-german-words'
-
-  one_page(path_begin)
-
-  for i in 3..12 do
-    full_path = path_begin + '-' + i.to_s
-    one_page(full_path)
+  domain = 'http://www.languagedaily.com'
+  pattern = 'http://www.languagedaily.com/learn-german/vocabulary/'
+  first_page = Nokogiri::HTML(open(domain + '/learn-german/vocabulary/common-german-words'))
+  all_links = []
+  first_page.css('a').each do |link|
+    full_path = domain + link['href'].to_s
+    all_links.push(full_path) if full_path.include? pattern
+  end
+  all_links.first(12).each do |link|
+    one_page(link)
   end
 end
 
