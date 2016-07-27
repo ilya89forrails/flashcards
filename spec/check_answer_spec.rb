5a10ae261d2e59b67b9a529a5ef3ae1146da32ad
ilya <ilya89forrails@gmail.com>
2016-07-27 23:39:17 +0900

    lesson6

--- spec/check_answer_spec.rb
+++ spec/check_answer_spec.rb
@@ -4,7 +4,7 @@ describe CheckAnswer do
   it 'pass - equity of user answer and translated text - CheckAnswer' do
     @card = Card.new(original_text: 'schwarz', translated_text: 'black', review_date: Date.today)
     user_answer = 'schwarz'
-    interactor = CheckAnswer.call(card: @card, answer: user_answer)
-    expect(interactor).to be_a_success
+    result = CheckAnswer.call(card: @card, answer: user_answer)
+    expect(result).to be_a_success
   end
 end
