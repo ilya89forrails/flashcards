91712c9499f1cb35dafe77bdf442b81298250b7b
ilya <ilya89forrails@gmail.com>
2016-08-01 20:18:45 +0900

    lesson8++

--- db/migrate/20160801085449_add_users.rb
+++ db/migrate/20160801085449_add_users.rb
@@ -1,6 +1,6 @@
 class AddUsers < ActiveRecord::Migration[5.0]
   def change
-  	create_table :users do |t|
+    create_table :users do |t|
       t.string   :email
       t.string   :password
       t.timestamps
--- db/migrate/20160801085513_add_user_id_column_to_cards.rb
+++ db/migrate/20160801085513_add_user_id_column_to_cards.rb
@@ -1,5 +1,5 @@
 class AddUserIdColumnToCards < ActiveRecord::Migration[5.0]
   def change
-  	add_column :cards, :user_id, :integer
+    add_column :cards, :user_id, :integer
   end
 end
